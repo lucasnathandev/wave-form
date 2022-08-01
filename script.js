c9e47d0aaf5652f35f6b2db35afbf05ab602f369
Solium Dev <lucas.develop.prog@gmail.com>
2022-08-01 00:39:10 -0300

    :tada: Error exception included in project and finished.

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 const labels = document.querySelectorAll(".label")
+const inputs = {
+  email: document.querySelector("#email"),
+  password: document.querySelector("#password"),
+}
+const loginBtn = document.querySelector(".btn")
 
-function main() {
+function showErrorSpan(e, message) {
+  const span = document.createElement("span")
+  span.textContent = message
+  span.className = "error"
+  e.target.parentElement.appendChild(span)
+  setTimeout(() => {
+    e.target.parentElement.removeChild(span)
+  }, 5000)
+}
+
+function sendHandler(e) {
+  // Please add an bcrypt like lib for password encrypting.
+  e.preventDefault()
+  const { email, password } = inputs
+  if (email.value.match("@") && email.value.endsWith(".com")) {
+    if (password.length > 6) {
+      return fetch("http://hostname/test", {
+        body: {
+          email: email.value,
+          password: password.value,
+        },
+        method: "post",
+      })
+    }
+
+    return showErrorSpan(e, "Password must be longer than 6 characters")
+  }
+  return showErrorSpan(e, "Email field must be format: abcdefg@abcdefg.com")
+}
+function addStyleTransition() {
   labels.forEach((label) => {
     label.innerHTML = label.textContent
       .split("")
@@ -10,6 +44,11 @@ function main() {
       )
       .join("")
   })
+}
+
+function main() {
+  loginBtn.addEventListener("pointerup", sendHandler)
+  addStyleTransition()
 }
 
 main()
